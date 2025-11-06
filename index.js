3dbc37cc84a426251d6b24c344bf9177b6e2f7ec
Alanda <alandacherestal@gmail.com>
2025-11-06 16:24:48 -0500

    added code to index.js

--- index.js
+++ index.js
@@ -1,4 +1,36 @@
 //Write your code here
+const attendee = {
+  attendeeId: "T001",
+  name: "Alice Smith",
+  event: "JavaScript Conference",
+  ticketType: "VIP",
+  ticketPrice: 150.00
+}
+
+function logAttendeeName(attendee){
+  console.log(attendee.name);
+}
+
+function logTicketPrice(attendee){
+  console.log(attendee.ticketPrice);
+}
+
+function updateTicketType(attendee, newticketType){
+  attendee.ticketType = newticketType;
+}
+
+function updateTicketPrice(attendee, newticketPrice){
+  attendee.ticketPrice = newticketPrice;
+}
+
+function removeEventProperty(attendee){
+  delete attendee.event;
+}
+
+function addCheckedInProperty(attendee){
+  attendee.checkedIn = true;
+}
+
 
 
 
